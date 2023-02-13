1a79598cff90b5df39eba4b7011a695e3b52d768
FarhanKhan7749 <fk828633@gmail.com>
2023-02-13 16:27:56 +0530

    Making the Items visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./component/Layout/Header";
-import MealsSummary from "./component/Meals/MealsSummary";
+import Meals from "./component/Meals/Meals";
+//import MealsSummary from "./component/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <MealsSummary />
+      <main>
+        <Meals></Meals>
+      </main>
     </React.Fragment>
   );
 }
